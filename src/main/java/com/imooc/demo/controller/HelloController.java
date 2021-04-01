6e2e0c3fd58b41a1d5d13125a55ff722f5e1dd45
fj <xjxafj@126.com>
2021-04-01 15:21:25 +0800

    first commit

---
+++ src/main/java/com/imooc/demo/controller/HelloController.java
@@ -0,0 +1,30 @@
+package com.imooc.demo.controller;
+
+import com.imooc.demo.dto.UserDto;
+import com.imooc.demo.service.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class HelloController {
+
+    @Resource
+    private UserService userService;
+
+    @RequestMapping("/")
+    public String index() {
+        //todo 等待开发完成
+        UserDto userDto = userService.doLogin(null);
+
+        System.out.println(123);
+        return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        System.out.println(123);
+        return "Greetings from Spring Boot!";
+    }
+}
